937e4223622192ea50fa28e58b485cc32416269a
l3lackJack <gabrielionuturs@iesebre.com>
2021-11-04 16:41:19 +0100

    Acabant videos

--- routes/web.php
+++ routes/web.php
@@ -1,27 +1,42 @@
 <?php
 
+use App\Models\Task;
 use Illuminate\Support\Facades\Route;
 
 
 Route::get('/', function () {
-    $task1 = new stdClass();
-    $task1->id = 1;
-    $task1->title = "Task 1";
-    $task1->description = "task 1";
-    $task1->completed = 1;
-    $task2 = new stdClass();
-    $task2->id = 1;
-    $task2->title = "Task 1";
-    $task2->description = "task 2";
-    $task2->completed = 1;
-
-
-    $tasks = [
-      $task1,
-        $task2
-    ];
+//    $task1 = Task::create([
+//        'title' => 'Tasca 1',
+//        'description' => 'lorem blabla',
+//        'completed'=> 0
+//    ]);
+//
+//    $task2 = Task::create([
+//        'title' => 'Tasca 2',
+//        'description' => 'lorem blabla',
+//        'completed'=> 1
+//    ]);
+//
+//    $task3 = Task::create([
+//        'title' => 'Tasca 3',
+//        'description' => 'lorem blabla',
+//        'completed'=> 0
+//    ]);
+//
+//    $task4 = Task::create([
+//        'title' => 'Tasca 4',
+//        'description' => 'Hola',
+//        'completed'=> 1
+//    ]);
+//
+//
+//    $tasks = [
+//      $task1,
+//        $task2,
+//        $task3
+//    ];
     return view('tasks',[
-        'tasks' => $tasks
+        'tasks' => Task::all()
     ]);
 });
 Route::get('/tasks', function () {
